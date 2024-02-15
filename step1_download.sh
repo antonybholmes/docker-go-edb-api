01cc4856c51e81a9b2b0d3e5e47382b8b84c9efe
Antony Holmes <antony@antonyholmes.dev>
2024-02-15 17:43:58 -0500

    fix: Bug fixes and updates.

--- step1_download.sh
+++ step1_download.sh
@@ -4,6 +4,8 @@ git clone git@github.com:antonybholmes/go-loctogene.git
 git clone git@github.com:antonybholmes/go-gene.git
 git clone git@github.com:antonybholmes/go-auth.git
 git clone git@github.com:antonybholmes/go-math.git
+git clone git@github.com:antonybholmes/go-env.git
+
 
 pwd=`pwd`
 for d in `find . -maxdepth 1 -type d`
