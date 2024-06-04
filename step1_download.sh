f8c3b814b18d87efa466d5b511f6b62a64b87e51
Antony Holmes <antony@antonyholmes.dev>
2024-06-04 13:51:21 -0400

    fix: Added gene conversion

--- step1_download.sh
+++ step1_download.sh
@@ -5,6 +5,9 @@ git clone git@github.com:antonybholmes/go-auth.git
 git clone git@github.com:antonybholmes/go-math.git
 git clone git@github.com:antonybholmes/go-sys.git
 git clone git@github.com:antonybholmes/go-mailer.git
+git clone git@github.com:antonybholmes/go-mutations.git
+git clone git@github.com:antonybholmes/go-gene-conversion.git
+
 
 pwd=`pwd`
 for d in `find . -maxdepth 1 -type d`
