168b7159ca7334d1021c26c5300a2335f049a6d3
Antony Holmes <antony@antonyholmes.dev>
2024-06-04 16:09:47 -0400

    fix: Added generate keys

--- step2_build.sh
+++
@@ -1,5 +0,0 @@
-scp -i ~/.ssh/rdf_virginia_ec2_key_pair.pem -r data/* ec2-user@ec2-54-89-118-252.compute-1.amazonaws.com:docker-go-edb-api/data
-
-# build in dev image
-docker build --tag go-edb-api:multistage .
-
--- step3_rebuild_edb-api.sh
+++ step3_rebuild_edb-api.sh
@@ -1,2 +1,9 @@
+# generate keys if they don't exist
+
+if [[ ! -f "jwtRS256.key" ]]
+then
+	./go-edb-api/create_rsa_keys.sh
+fi
+
 # rebuild in background then replace once built
 docker-compose up -d --no-deps --build edb-api
